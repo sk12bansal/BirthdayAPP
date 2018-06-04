9c22a3f30ae55134df129a8da1dab5570159c4c5
surakum2 <surakum2@cisco.com>
2018-06-04 18:00:40 +0530

    update_login_page

--- app/src/main/java/com/sk/applytheme/app/SignupFragment.java
+++ app/src/main/java/com/sk/applytheme/app/SignupFragment.java
@@ -6,14 +6,51 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.sk.applytheme.R;
 
-public class SignupFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SignupFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.signup_fragment,container,false);
+
+        // Spinner element
+        Spinner spinner = (Spinner)view.findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("+91 India");
+        categories.add("+1 US");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
